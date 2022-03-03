28f090c8ca0664f3ee8ec756253b70ba382934d6
xiaoming <zym13937090628@gmail.com>
2022-03-03 17:05:02 +0800

    git commit 2

---
+++ src/main/java/org/atchongda/git/GitTest.java
@@ -0,0 +1,13 @@
+package org.atchongda.git;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @author xiaomingtongxue
+ * @create 2022-03-03-17:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("明哥真帅");
+    }
+}
