097bf4e6b74e3daf2646954bdb980b5154bccdb4
xiaoming <zym13937090628@gmail.com>
2022-03-03 17:19:45 +0800

    git commit 3

--- src/main/java/org/atchongda/git/GitTest.java
+++ src/main/java/org/atchongda/git/GitTest.java
@@ -9,5 +9,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("明哥真帅");
+        System.out.println("明哥真帅,使用SSH push");
     }
 }
